8035f867254b4634f75386e2c46c1158eb6574b6
Cuchador <willmiranda2002@gmail.com>
2022-06-09 17:40:47 -0500

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import suit_pic from "../assets/suit_pic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p><div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          src={suit_pic} alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Will Miranda</div>
+        <div className="brief_description">
+          Computer Science | Texas A&M University
+        </div>
+      </div>
+    </div>
+  </div></p> 
       </div>
     )
   }
