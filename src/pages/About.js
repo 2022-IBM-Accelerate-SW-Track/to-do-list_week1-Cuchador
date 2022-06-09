b0855b94829de0605822cfc79e858358b9beb915
Cuchador <106778313+Cuchador@users.noreply.github.com>
2022-06-08 19:03:15 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,15 +10,18 @@ export default class About extends Component {
         <img 
           className="profile_image"
           // Image goes here
-          alt="Profile Pic"
+          src="suit_pic.png" alt="Profile Pic"
           ></img>
       </div>
     </div>
     <div className="split right">
       <div className="centered">
-        <div className="name_title">Your Name</div>
+        <div className="name_title">Will Miranda</div>
         <div className="brief_description">
           // Details about you goes here
+          Howdy! My name is Will, and I am a Computer Science major at Texas A&M University in College Station.
+          In my free time I like to workout, cook (and eat), play games, and hang out with my roommates.
+          In the future I'd like to work as a Software Developer!
         </div>
       </div>
     </div>
